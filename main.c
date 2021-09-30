d30c1c50973800db7591d1f8bbf0904003cfb5fe
pipiduck <67134787+pipiduck@users.noreply.github.com>
2021-09-30 16:20:47 +0800

    clear redundant

--- main.c
+++ main.c
@@ -5,17 +5,13 @@
  * @Date: 2021-09-30 16:00:48
  * @Feature:  
  * @LastEditors: wsy
- * @LastEditTime: 2021-09-30 16:07:19
+ * @LastEditTime: 2021-09-30 16:20:19
  * @FilePath: \learning-git-homework\main.c
  */
 hello pipiduck!
 D
 E
 F
- * @LastEditTime: 2021-09-30 16:14:09
- * @FilePath: \learning-git-homework\main.c
- */
-hello pipiduck!
 A
 B
 c
