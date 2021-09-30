f72e12e5db9627a6f6ff6346559aa2b24a991faf
pipiduck <67134787+pipiduck@users.noreply.github.com>
2021-09-30 16:01:49 +0800

    init

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Autor: wangshiya
+ * @Date: 2021-09-30 16:00:48
+ * @Feature:  
+ * @LastEditors: wsy
+ * @LastEditTime: 2021-09-30 16:00:48
+ * @FilePath: \learning-git-homework\main.c
+ */
+hello pipiduck!
